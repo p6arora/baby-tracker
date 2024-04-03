e4837d1b6771343130acda092946082aa05512c4
Paarth Arora <paarth.arora21@gmail.com>
2024-04-02 21:28:38 -0400

    first commit

---
+++ backend/app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Canadian1!@localhost/baby-tracker'
+db = SQLAlchemy(app)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+
+    def __repr__(self) -> str:
+        return f"Event: {self.description}"
+    
+    def __init__(self, description):
+        self.description = description
+
+
+@app.route('/')
+def hello():
+    return 'Hey!'
+
+# create an event
+@app.route('/events', methods=['POST'])
+def create_event():
+    description = request.json['description']
+    event = Event(description)
+    db.session.add(event)
+    db.session.commit()
+    return format_event(event)
+
+# get all events
+@app.route('/events', methods=['GET'])
+def get_events():
+    events = Event.query.order_by(Event.id.asc()).all()
+    event_list = []
+    for event in events:
+        event_list.append(format_event(event))
+    return {'events': event_list}
+
+# get single event
+@app.route('/events/<id>', methods=['GET'])
+def get_event(id):
+    event = Event.query.filter_by(id=id).one()
+    formatted_event = format_event(event)
+    return formatted_event
+
+# delete an event
+@app.route('/events/<id>', methods=['DELETE'])
+def delete_event(id):
+    event = Event.query.filter_by(id=id).one()
+    db.session.delete(event)
+    db.session.commit()
+    return f'Event {id} Deleted'
+
+# edit an event
+@app.route('/events/<id>', methods=['PUT'])
+def update_event(id):
+    event = Event.query.filter_by(id=id)
+    description = request.json['description']
+    event.update(dict(description = description, created_at = datetime.now(timezone.utc)))
+    db.session.commit()
+    return {'events': format_event(event.one())}
+
+
+def format_event(event):
+    return {
+        "description": event.description,
+        "id": event.id,
+        "created_at": event.created_at
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
